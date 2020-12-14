2a2cfae785286731bc61fea5a769c9b2db75f56f
TUTOR03 <artemss20030906@gmail.com>
2020-12-15 01:54:09 +0500

    added basic SideBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import React, { useEffect } from "react";
 import { useTimetable } from "./hooks";
-
+import Sidebar from './components/SideBar'
 import styles from "./styles/App.module.css";
 
 const App: React.FC = () => {
-  const { createDay } = useTimetable();
-
-  useEffect(() => createDay(1, 2, 502), []);
 
   return (
-    <div className="wrapper">
-      <nav id="sidebar">
-        <h1 className="display-1">akdakdlk</h1>
-        <p className={styles.tastiestTest}>Testing text</p>
-        <span className={styles.title}>Title</span>
-      </nav>
+    <div className={styles.wrapper}>
+      <Sidebar/>
     </div>
   );
 };
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "../styles/Sidebar.module.css";
+
+const Sidebar: React.FC = () => {
+	console.log(styles)
+  return (
+    <nav id={styles.sidebar} className={styles.active}>
+    	<div>
+        <h3>Bootstrap Sidebar</h3>
+      </div>
+      <button type="button" id={styles.sidebarToggle} className="btn btn-secondary rounded-end">X</button>
+    </nav>
+  );
+};
+
+export default Sidebar;

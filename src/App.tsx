0666b1438c2cc0114f94693b7de0e9d3189a20a2
TUTOR03 <TUTOR03@users.noreply.github.com>
2020-12-14 20:55:20 +0000

    Apply Prettier styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect } from "react";
 import { useTimetable } from "./hooks";
-import Sidebar from './components/SideBar'
+import Sidebar from "./components/SideBar";
 import styles from "./styles/App.module.css";
 
 const App: React.FC = () => {
-
   return (
     <div className={styles.wrapper}>
-      <Sidebar/>
+      <Sidebar />
     </div>
   );
 };
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import styles from "../styles/Sidebar.module.css";
 
 const Sidebar: React.FC = () => {
-	console.log(styles)
+  console.log(styles);
   return (
     <nav id={styles.sidebar} className={styles.active}>
-    	<div>
+      <div>
         <h3>Bootstrap Sidebar</h3>
       </div>
-      <button type="button" id={styles.sidebarToggle} className="btn btn-secondary rounded-end">X</button>
+      <button
+        type="button"
+        id={styles.sidebarToggle}
+        className="btn btn-secondary rounded-end"
+      >
+        X
+      </button>
     </nav>
   );
 };

132c330eafc42714be3d6e993e499b76ebc685ba
dm1sh <me@dmitriy.icu>
2020-12-16 12:32:34 +0500

    Fixed some code styling issues

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,13 +1,18 @@
 import React, { createRef } from "react";
-import styles from "../styles/Sidebar.module.css";
+import styles from "../styles/SideBar.module.css";
 
 const SideBar: React.FC = () => {
   const sideBarRef = createRef<HTMLDivElement>();
+
   const toggleSideBar = (event: React.MouseEvent) => {
     event.preventDefault();
-    const sidebar = sideBarRef.current!;
-    sidebar.classList.toggle(styles.active);
+
+    if (sideBarRef.current) {
+      const sidebar = sideBarRef.current;
+      sidebar.classList.toggle(styles.active);
+    }
   };
+
   return (
     <nav id={styles.sidebar} ref={sideBarRef}>
       <div>
@@ -28,7 +33,7 @@ const SideBar: React.FC = () => {
           viewBox="0 0 16 16"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8zm-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5z"
           />
         </svg>

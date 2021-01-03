d23115ebbea2e13155739cdd066aca46f930733c
TUTOR03 <TUTOR03@users.noreply.github.com>
2021-01-03 22:17:54 +0000

    Apply formatting changes

--- src/components/Constructor.tsx
+++ src/components/Constructor.tsx
@@ -1,84 +1,89 @@
 import React, { useState } from "react";
 import ConstructorPage from "./ConstructorPage";
 import styles from "../styles/Constructor.module.css";
-import { DayT, CreateDayFT } from "../types/timetable"
+import { DayT, CreateDayFT } from "../types/timetable";
 
 type ConstructorProps = {
-	constructorRef: React.RefObject<HTMLDivElement>;
-	createDay: CreateDayFT;
-	days: DayT[];
+  constructorRef: React.RefObject<HTMLDivElement>;
+  createDay: CreateDayFT;
+  days: DayT[];
 };
 
 const Constructor: React.FC<ConstructorProps> = ({ constructorRef, createDay, days }) => {
-	const [pageState, setPageState] = useState<number>(0)
-	const [startDateState, setStartDateState] = useState<Date>(new Date())
+  const [pageState, setPageState] = useState<number>(0);
+  const [startDateState, setStartDateState] = useState<Date>(new Date());
 
-	const changePage = (event: React.MouseEvent<HTMLElement>) => {
-		event.preventDefault();
-		if (event.currentTarget.dataset.name) {
-			const name = parseInt(event.currentTarget.dataset.name);
-			setPageState(name);
-		}
-	};
+  const changePage = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    if (event.currentTarget.dataset.name) {
+      const name = parseInt(event.currentTarget.dataset.name);
+      setPageState(name);
+    }
+  };
 
-	const changeDate = (event: React.ChangeEvent<HTMLInputElement>) => {
-		event.preventDefault();
-		if (event.currentTarget.value) {
-			setStartDateState(new Date(event.currentTarget.value));
-		}
-	};
+  const changeDate = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    if (event.currentTarget.value) {
+      setStartDateState(new Date(event.currentTarget.value));
+    }
+  };
 
-	const addButton = (event: React.MouseEvent<HTMLButtonElement>) => {
-		event.preventDefault()
-		if (event.currentTarget.dataset.name) {
-			const name = event.currentTarget.dataset.name
-			console.log(new Date(startDateState.getTime() + 24 * 3600 * 1000 * days.length))
-			if (name == 'day') {
-				createDay(startDateState.getTime() + 24 * 3600 * 1000 * days.length)
-			}
-		}
-	}
+  const addButton = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if (event.currentTarget.dataset.name) {
+      const name = event.currentTarget.dataset.name;
+      console.log(new Date(startDateState.getTime() + 24 * 3600 * 1000 * days.length));
+      if (name == "day") {
+        createDay(startDateState.getTime() + 24 * 3600 * 1000 * days.length);
+      }
+    }
+  };
 
-	return (
-		<div id={styles.constructorPages} ref={constructorRef}>
-			<ul className="nav nav-tabs pt-2">
-				<li className={`nav-item ${styles.pageItem}`}>
-					<a onClick={changePage} data-name='0' className={`nav-link ${pageState == 0 ? 'active' : ''}`}>
-						10 класс
+  return (
+    <div id={styles.constructorPages} ref={constructorRef}>
+      <ul className="nav nav-tabs pt-2">
+        <li className={`nav-item ${styles.pageItem}`}>
+          <a
+            onClick={changePage}
+            data-name="0"
+            className={`nav-link ${pageState == 0 ? "active" : ""}`}
+          >
+            10 класс
           </a>
-				</li>
-				<li className={`nav-item ${styles.pageItem}`}>
-					<a
-						onClick={changePage}
-						data-name="1"
-						className={`nav-link ${pageState == 1 ? "active" : ""}`}
-					>
-						11 класс
+        </li>
+        <li className={`nav-item ${styles.pageItem}`}>
+          <a
+            onClick={changePage}
+            data-name="1"
+            className={`nav-link ${pageState == 1 ? "active" : ""}`}
+          >
+            11 класс
           </a>
-				</li>
-				<form className='d-flex align-items-center ms-auto me-2'>
-					<div className="input-group input-group">
-						<label htmlFor="startDate" className="form-label mb-0 pt-1 me-2">Дата начала недели:</label>
-						<input
-							type="date"
-							value={`${startDateState.toISOString().split('T')[0]}`}
-							onChange={changeDate}
-							id='startDate'
-							className="form-control rounded rounded-3"
-						/>
-					</div>
-				</form>
-			</ul>
-			<div className='container-fluid'>
-				{
-					pageState == 0 ?
-						<ConstructorPage days={days} addButton={addButton} classNum={'lessons10'} />
-						:
-						''
-				}
-			</div>
-		</div>
-	);
+        </li>
+        <form className="d-flex align-items-center ms-auto me-2">
+          <div className="input-group input-group">
+            <label htmlFor="startDate" className="form-label mb-0 pt-1 me-2">
+              Дата начала недели:
+            </label>
+            <input
+              type="date"
+              value={`${startDateState.toISOString().split("T")[0]}`}
+              onChange={changeDate}
+              id="startDate"
+              className="form-control rounded rounded-3"
+            />
+          </div>
+        </form>
+      </ul>
+      <div className="container-fluid">
+        {pageState == 0 ? (
+          <ConstructorPage days={days} addButton={addButton} classNum={"lessons10"} />
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Constructor;
--- src/components/ConstructorPage.tsx
+++ src/components/ConstructorPage.tsx
@@ -3,17 +3,24 @@ import styles from "../styles/Constructor.module.css";
 import { DayT } from "../types/timetable";
 
 type ConstructorPageProps = {
-	days: DayT[];
-	classNum: string;
-	addButton: (event: React.MouseEvent<HTMLButtonElement>) => void;
-}
+  days: DayT[];
+  classNum: string;
+  addButton: (event: React.MouseEvent<HTMLButtonElement>) => void;
+};
 
 const ConstructorPage: React.FC<ConstructorPageProps> = ({ days, classNum, addButton }) => {
-	return (
-		<div className="d-grid gap-2 my-2">
-			<button data-name='day' onClick={addButton} className="btn btn-outline-primary btn-sm" type="button">Новый День</button>
-		</div>
-	);
+  return (
+    <div className="d-grid gap-2 my-2">
+      <button
+        data-name="day"
+        onClick={addButton}
+        className="btn btn-outline-primary btn-sm"
+        type="button"
+      >
+        Новый День
+      </button>
+    </div>
+  );
 };
 
 export default ConstructorPage;

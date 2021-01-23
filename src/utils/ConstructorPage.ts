334077b683ad9fd743e8d80e0a1f95b6d18e69c6
TUTOR03 <artemss20030906@gmail.com>
2021-01-23 13:16:46 +0500

    added title to select

---
+++ src/utils/ConstructorPage.ts
@@ -0,0 +1,10 @@
+import { SubjectT, CardT } from "../types/timetable"
+
+export const generateTitle = (subjects: SubjectT[], cards: CardT[], teachers: string[], value: number) => {
+	if(value!=-1){
+		return(`${subjects[cards[value].subject].title} - ${teachers[cards[value].teacher]} - ${cards[value].room}`)
+	}
+	else{
+		return('')
+	}
+}
